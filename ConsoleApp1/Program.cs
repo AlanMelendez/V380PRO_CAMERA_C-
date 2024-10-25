83eaf28271b51ef3b100e64b04bd6074e7c48b40
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-10-24 20:59:07 -0600

    Apply clean code to program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,8 @@ namespace ConsoleApp1
     {
         static async Task Main(string[] args)
         {
-            await CaptureVideoNotFile();
+            await CaptureVideo(true);
         }
-
 
         static async Task SendRequestAsync(string xmlFile)
         {
@@ -42,155 +41,75 @@ namespace ConsoleApp1
             }
         }
 
-        static async Task CaptureVideoInFile()
+        static async Task CaptureVideo(bool saveToFile)
         {
-            // Captura de video RTSP
             string rtspUrl = "rtsp://192.168.100.19/live/ch00_1";
             using (var capture = new VideoCapture(rtspUrl))
             {
-                Mat frame = new Mat();
-
                 if (!capture.IsOpened())
                 {
                     Console.WriteLine("No se pudo abrir la cámara.");
                     return;
                 }
 
-                Cv2.NamedWindow("IonicAppCamera", WindowFlags.Normal);
-
-                // Obtener el tamaño del frame
-                Size frameSize = new Size(capture.FrameWidth, capture.FrameHeight);
-
-                // Definir el VideoWriter
-                using (VideoWriter writer = new VideoWriter("video.avi", FourCC.XVID, 30, frameSize))
-                {
-                    bool isPtzCommandRunning = false; // Evitar múltiples solicitudes PTZ simultáneas hacia la cámara.
-
-                    while (true)
-                    {
-                        // Leer el siguiente fotograma de la cámara
-                        if (!capture.Read(frame))
-                        {
-                            Console.WriteLine("No se pudo capturar el frame.");
-                            break;
-                        }
-
-                        // Comprobar si el frame está vacío
-                        if (frame.Empty())
-                        {
-                            Console.WriteLine("El frame está vacío.");
-                            break;
-                        }
-
-                        // Mostrar el fotograma en la ventana
-                        Cv2.ImShow("IonicAppCamera", frame);
-
-                        // Escribir el fotograma al archivo de video
-                        writer.Write(frame);
-
-                        // Esperar 10 ms y verificar si se ha presionado una tecla
-                        int key = Cv2.WaitKey(10);
-
-                        if (key == 105 && !isPtzCommandRunning) // 105 = 'i' (tecla 'i')
-                        {
-                            isPtzCommandRunning = true;
-                            _ = Task.Run(async () =>
-                            {
-                                // Control PTZ con solicitudes HTTP en segundo plano
-                                string xmlFilePath = @"C:\Users\Usuario\source\repos\ConsoleApp1\ConsoleApp1\xml\postup.xml";
-                                await SendRequestAsync(xmlFilePath);
-                                isPtzCommandRunning = false; // Marcar como finalizado
-                            });
-                        }
-                        else if (key == 44 && !isPtzCommandRunning) // 44 = ',' (tecla ',')
-                        {
-                            isPtzCommandRunning = true;
-                            _= Task.Run(async () =>
-                            {
-                                // Control PTZ con solicitudes HTTP en segundo plano
-                                string xmlFilePath = @"C:\Users\Usuario\source\repos\ConsoleApp1\ConsoleApp1\xml\postdown.xml";
-                                await SendRequestAsync(xmlFilePath);
-                                isPtzCommandRunning = false; // Marcar como finalizado
-                            });
-                        }
-                        else if (key == 27) // 27 = 'ESC' (para salir del bucle)
-                        {
-                            Console.WriteLine("Saliendo...");
-                            break;
-                        }
-                    }
-                }
-
-                // Liberar los recursos
-                Cv2.DestroyAllWindows();
-            }
-        }
-        static async Task CaptureVideoNotFile()
-        {
-            // Captura de video RTSP
-            string rtspUrl = "rtsp://192.168.100.19/live/ch00_1";
-            using (var capture = new VideoCapture(rtspUrl))
-            {
                 Mat frame = new Mat();
-
-                if (!capture.IsOpened())
-                {
-                    Console.WriteLine("Don't open the camera.");
-                    return;
-                }
-
                 Cv2.NamedWindow("IonicAppCamera", WindowFlags.Normal);
 
-                bool isPtzCommandRunning = false; // Para evitar múltiples solicitudes PTZ simultáneas hacia la camara.
+                VideoWriter writer = null;
+                if (saveToFile)
+                {
+                    Size frameSize = new Size(capture.FrameWidth, capture.FrameHeight);
+                    writer = new VideoWriter("video.avi", FourCC.XVID, 30, frameSize);
+                }
 
+                bool isPtzCommandRunning = false;
                 while (true)
                 {
-                    // Leer el siguiente fotograma de la cámara
-                    if (!capture.Read(frame))
+                    if (!capture.Read(frame) || frame.Empty())
                     {
                         Console.WriteLine("No se pudo capturar el frame.");
                         break;
                     }
 
-                    // Mostrar el fotograma en la ventana
                     Cv2.ImShow("IonicAppCamera", frame);
 
-                    // Esperar 10 ms y verificar si se ha presionado una tecla
+                    if (saveToFile)
+                    {
+                        writer?.Write(frame);
+                    }
+
                     int key = Cv2.WaitKey(10);
-
-                    if (key == 105 && !isPtzCommandRunning) // 105 = 'i' (tecla 'i')
-                    {
-                        isPtzCommandRunning = true;
-                        _ = Task.Run(async () =>
-                        {
-                            // Control PTZ con solicitudes HTTP en segundo plano
-                            string xmlFilePath = @"C:\Users\Usuario\source\repos\ConsoleApp1\ConsoleApp1\xml\postup.xml";
-                            await SendRequestAsync(xmlFilePath);
-                            isPtzCommandRunning = false; // Marcar como finalizado
-                        });
-                    }
-                    else if (key == 44 && !isPtzCommandRunning) // 44 = ',' (tecla ',')
-                    {
-                        isPtzCommandRunning = true;
-                        _= Task.Run(async () =>
-                        {
-                            // Control PTZ con solicitudes HTTP en segundo plano
-                            string xmlFilePath = @"C:\Users\Usuario\source\repos\ConsoleApp1\ConsoleApp1\xml\postdown.xml";
-                            await SendRequestAsync(xmlFilePath);
-                            isPtzCommandRunning = false; // Marcar como finalizado
-                        });
-                    }
-                    else if (key == 27) // 27 = 'ESC' (para salir del bucle)
-                    {
-                        Console.WriteLine("Saliendo...");
-                        break;
-                    }
-
+                    if (HandleKeyPress(key, ref isPtzCommandRunning)) break;
                 }
-            }
 
-            // Liberar los recursos
-            Cv2.DestroyAllWindows();
+                writer?.Release();
+                Cv2.DestroyAllWindows();
+            }
+        }
+
+        static bool HandleKeyPress(int key, ref bool isPtzCommandRunning)
+        {
+            if (key == 105 && !isPtzCommandRunning) // 'i' for PTZ up
+            {
+                ExecutePtzCommandAsync(@"C:\path\to\postup.xml", isPtzCommandRunning);
+            }
+            else if (key == 44 && !isPtzCommandRunning) // ',' for PTZ down
+            {
+                ExecutePtzCommandAsync(@"C:\path\to\postdown.xml", isPtzCommandRunning);
+            }
+            else if (key == 27) // ESC to exit
+            {
+                Console.WriteLine("Saliendo...");
+                return true;
+            }
+            return false;
+        }
+
+        static async Task ExecutePtzCommandAsync(string xmlFilePath, bool isPtzCommandRunning)
+        {
+            isPtzCommandRunning = true;
+            await SendRequestAsync(xmlFilePath);
+            isPtzCommandRunning = false;
         }
     }
 }
